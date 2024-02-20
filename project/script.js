f323a5644c6d737b5ed38500dc5e5bc9554f4191
Nadav Goldin <120043799+ndvp39@users.noreply.github.com>
2024-02-20 20:17:23 +0200

    Delete project/script.js

--- project/script.js
+++
@@ -1,6 +0,0 @@
-function performSearch() {
-    var searchString = document.getElementById('searchInput').value;
-
-    // Send the search string to Python using output module
-    google.colab.kernel.invokeFunction('notebook.receive_search_string', [searchString], {});
-}

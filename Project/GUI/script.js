f3e5bfa95c125fd45447681ede2836841e95325a
daniarmag <75178381+daniarmag@users.noreply.github.com>
2024-03-26 14:50:31 +0200

    Finishes

--- Project/GUI/script.js
+++ Project/GUI/script.js
@@ -54,7 +54,7 @@ function ChatbotResponse(response) {
     
     // Append chatbot's response to the chat
     var chatContainer = document.getElementById('chat');
-    chatContainer.innerHTML += '<div><strong style="color: blue;">Chatbot:</strong> ' + response + '</div>';
+    chatContainer.innerHTML += '<div><strong style="color: #3498db;">Chatbot:</strong> ' + response + '</div>';
     chatContainer.innerHTML += '<br>'; 
     // Clear the user input
     document.getElementById('user-input-chatbot').value = '';
